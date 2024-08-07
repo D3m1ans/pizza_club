add6d082cc7d57c4d9d5d8d9625910ae3a3e654d
ne1ro <jack.20033505@gmail.com>
2024-08-07 15:34:35 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from dotenv import find_dotenv, load_dotenv
+from aiogram.types import BotCommandScopeAllPrivateChats
+
+from aiogram.enums import ParseMode
+
+
+load_dotenv(find_dotenv())
+
+from middlewares.db import DataBaseSession
+
+from database.engine import create_db, drop_db, session_maker
+
+from handlers.user_private import user_private_router
+from handlers.user_grope import user_group_router
+from handlers.admin_private import admin_router
+
+# from common.bot_cmds_list import private
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+bot.my_admins_list = []
+
+dp = Dispatcher()
+
+dp.include_router(user_private_router)
+dp.include_router(user_group_router)
+dp.include_router(admin_router)
+
+async def on_startup(bot):
+
+    await drop_db()
+
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Shutting down...')
+
+async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+#    await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+asyncio.run(main())
