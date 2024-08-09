d52e3dbe820b5a364a96c3af18859718a65d6368
ne1ro <jack.20033505@gmail.com>
2024-08-09 23:44:01 +0300

    made main banner, but got bag in the catalog banner

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -4,79 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.orm_query import orm_get_products
 from filters.chat_types import ChatFilters
+from handlers.menu_processing import get_menu_content
 from keyboard.inline_board import get_callback_btns
 
 user_private_router = Router()
 user_private_router.message.filter(ChatFilters(['private']))
 
 @user_private_router.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.first_name}, я виртуальный помощник",
-                         reply_markup=get_callback_btns(btns={
-                             'Нажми на меня': 'some_1'
-                         }))
+async def start_cmd(message: types.Message, session: AsyncSession):
+    media, reply_markup = await get_menu_content(session, level=0, menu_name="main")
 
-@user_private_router.callback_query(F.data.startswith('some_'))
-async def counter(callback: types.CallbackQuery):
-    number = int(callback.data.split("_")[-1])
-
-    await callback.message.edit_text(
-        text=f'Нажатий: {number}', reply_markup=get_callback_btns(btns={
-            'Нажми еще': f'some_{number+1}'
-        })
-    )
-
-# @user_private_router.message(Command("start"))
-# async def start(message: types.Message):
-#     await message.answer(f"Добро пожаловать, {message.from_user.first_name}!", reply_markup=reply_board.start_kb)
-#
-# @user_private_router.message(F.text.lower() == 'меню')
-# @user_private_router.message(Command("menu"))
-# async def menu(message: types.Message, session: AsyncSession):
-#     for product in await orm_get_products(session):
-#         await message.answer_photo(
-#             product.image,
-#             caption=f"Товар: {product.name}\n"
-#                     f"Описание товара: {product.description}\n"
-#                     f"Цена: {round(product.price, 2)}"
-#         )
-#     await message.answer("Вот наше меню ⏫", reply_markup=reply_board.del_keyboard)
-#
-# @user_private_router.message(F.text.lower() == 'о магазине')
-# @user_private_router.message(Command("about"))
-# async def about(message: types.Message):
-#     await message.answer("Наше описание")
-#
-# @user_private_router.message((F.text.lower().contains('оплат') or F.text.lower() == 'варианты оплаты'))
-# @user_private_router.message(Command("payment"))
-# async def payment(message: types.Message):
-#     text = as_marked_section(
-#         "Варианты оплаты",
-#         "Картой в боте",
-#         "При получении карта/кеш",
-#         "В заведении",
-#         marker='✅ '
-#         )
-#     await message.answer(text.as_html())
-#
-# @user_private_router.message((F.text.lower().contains('доставк') or F.text.lower() == 'варианты доставки'))
-# @user_private_router.message(Command("shipping"))
-# async def shipping(message: types.Message):
-#     text = as_marked_section(
-#         "Варианты доставки:",
-#         "Курьер",
-#         "Самовывоз (сейчас прибегу, заберу)",
-#         "Покушаю у вас (сейчас прибегу)",
-#         marker='✅ '
-#     )
-#     await message.answer(text.as_html(), reply_markup=reply_board.shipping_kb)
-#
-# @user_private_router.message(F.contact)
-# async def get_contact(message: types.Message):
-#     await message.answer(f"номер получен")
-#     await message.answer(str(message.contact.phone_number))
-#
-# @user_private_router.message(F.location)
-# async def get_location(message: types.Message):
-#     await message.answer(f"локация получена")
-#     await message.answer(f'Локация: {str(message.location.latitude), str(message.location.longitude)}')
+    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
--- keyboard/inline_board.py
+++ keyboard/inline_board.py
@@ -1,3 +1,4 @@
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
@@ -38,5 +39,53 @@ def get_inlineMix_btns(
             keyboard.add(InlineKeyboardButton(text=text, url=value))
         else:
             keyboard.add(InlineKeyboardButton(text=text, callback_data=value))
+
+    return keyboard.adjust(*sizes).as_markup()
+
+
+class MenuCallBack(CallbackData, prefix='main'):
+    level: int
+    menu_name: str
+    category: int | None = None
+    page: int = 1
+    product_id: int | None = None
+
+
+
+def get_user_main_buttons(*, level: int, sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+    btns = {
+        "Товары 🍕": "catalog",
+        "Корзина 🛒": "cart",
+        "О нас ℹ️": "about",
+        "Оплата 💰": "payment",
+        "Доставка ⛵": "shipping",
+    }
+    for text, menu_name in btns.items():
+        if menu_name == 'catalog':
+            keyboard.add(InlineKeyboardButton(text=text,
+                                              callback_data=MenuCallBack(level=level + 1, menu_name=menu_name).pack()))
+        elif menu_name == 'cart':
+            keyboard.add(InlineKeyboardButton(text=text,
+                                              callback_data=MenuCallBack(level=3, menu_name=menu_name).pack()))
+        else:
+            keyboard.add(InlineKeyboardButton(text=text,
+                                              callback_data=MenuCallBack(level=level, menu_name=menu_name).pack()))
+
+    return keyboard.adjust(*sizes).as_markup()
+
+
+def get_user_catalog_buttons(*, level: int, categories: list, sizes: tuple[int] = (2,)):
+    keyboard = InlineKeyboardBuilder()
+
+    keyboard.add(InlineKeyboardButton(text='Назад',
+                                      callback_data=MenuCallBack(level=level - 1, menu_name='main').pack()))
+    keyboard.add(InlineKeyboardButton(text='Корзина 🛒',
+                                      callback_data=MenuCallBack(level=3, menu_name='cart').pack()))
+
+    for c in categories:
+        keyboard.add(InlineKeyboardButton(text=c.name,
+                                          callback_data=MenuCallBack(level=level + 1, menu_name=c.name,
+                                                                     category=c.id).pack()))
 
     return keyboard.adjust(*sizes).as_markup()
