12a1d5a095adcbf4556c2bc263bff4f24f28d7cc
ne1ro <jack.20033505@gmail.com>
2024-08-15 21:50:23 +0300

    complete bot, but 1 bag with cart

--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -5,11 +5,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.orm_query import (
     orm_get_banner,
-    orm_get_categories, orm_get_products, Paginator,
+    orm_get_categories, orm_get_products, Paginator, orm_delete_from_cart, orm_reduce_product_in_cart, orm_add_to_cart,
+    orm_get_user_carts,
 )
 from keyboard.inline_board import (
     get_user_catalog_btns,
-    get_user_main_btns, get_products_btns,
+    get_user_main_btns, get_products_btns, get_user_cart,
 )
 
 async def main_menu(session, level, menu_name):
@@ -69,6 +70,56 @@ async def products(session, level, category, page):
 
     return image, kbds
 
+async def carts(session, level, menu_name, page, user_id, product_id):
+    if menu_name == 'delete':
+        await orm_delete_from_cart(session, user_id, product_id)
+        if page > 1: page -= 1
+    elif menu_name == 'decrement':
+        is_cart = await orm_reduce_product_in_cart(session, user_id, product_id)
+        if page > 1 and not is_cart: page -= 1
+    elif menu_name == 'increment':
+        await orm_add_to_cart(session, user_id, product_id)
+
+    carts = await orm_get_user_carts(session, user_id)
+
+    if not carts:
+        banner = await orm_get_banner(session, 'cart')
+        image = InputMediaPhoto(media=banner.image, caption=banner.description)
+
+        kbds = get_user_cart(
+            level = level,
+            page=None,
+            pagination_btns=None,
+            product_id=None,
+        )
+
+    else:
+        paginator = Paginator(carts, page=page)
+
+        cart = paginator.get_page()
+        cart_0 = cart[0]
+
+        cart_price = round(cart_0.quantity * cart_0.product.price, 2)
+        total_price = round(sum(cart_0.quantity * cart_0.product.price for cart_0 in carts), 2)
+        image = InputMediaPhoto(
+            media=cart_0.product.image,
+            caption=f"Продукт: {cart_0.product.name}\n"
+                    f"Цена: {cart_0.product.price} * {cart_0.quantity} = {cart_price}\n"
+                    f"Товар {paginator.page} из {paginator.pages} в корзине.\n"
+                    f"Общая стоимость товаров в корзине: {total_price}",
+        )
+
+        paginator_btns = pages(paginator)
+
+        kbds = get_user_cart(
+            level = level,
+            page=page,
+            pagination_btns=paginator_btns,
+            product_id=cart_0.product.id,
+        )
+
+        return image, kbds
+
 async def get_menu_content(
     session: AsyncSession,
     level: int,
@@ -84,3 +135,5 @@ async def get_menu_content(
         return await catalog(session, level, menu_name)
     elif level == 2:
         return await products(session, level, category, page)
+    elif level == 3:
+        return await carts(session, level, menu_name, page, user_id, product_id)
--- keyboard/inline_board.py
+++ keyboard/inline_board.py
@@ -94,3 +94,50 @@ def get_products_btns(
                                                                                   category=category,
                                                                                   page=page - 1).pack()))
     return keyboard.row(*row).as_markup()
+
+def get_user_cart(
+        *,
+        level: int,
+        page: int | None,
+        pagination_btns: dict | None,
+        product_id: int | None,
+        sizes: tuple[int] = (3,)
+):
+    keyboard = InlineKeyboardBuilder()
+    if page:
+        keyboard.add(InlineKeyboardButton(text='Удалить',
+                                          callback_data=MenuCallBack(level=level, menu_name='delete',
+                                                                     product_id=product_id, page=page).pack()))
+        keyboard.add(InlineKeyboardButton(text='-1',
+                                          callback_data=MenuCallBack(level=level, menu_name='decrement',
+                                                                     product_id=product_id, page=page).pack()))
+        keyboard.add(InlineKeyboardButton(text='+1',
+                                          callback_data=MenuCallBack(level=level, menu_name='increment',
+                                                                     product_id=product_id, page=page).pack()))
+        keyboard.adjust(*sizes)
+
+        row = []
+        for text, menu_name in pagination_btns.items():
+            if menu_name == 'next':
+                row.append(InlineKeyboardButton(text=text,
+                                                callback_data=MenuCallBack(level=level, menu_name=menu_name,
+                                                                           page=page + 1).pack()))
+            elif menu_name == 'previous':
+                row.append(InlineKeyboardButton(text=text,
+                                                callback_data=MenuCallBack(level=level, menu_name=menu_name,
+                                                                           page=page - 1).pack()))
+        keyboard.row(*row)
+
+        row2 = [
+            InlineKeyboardButton(text='На главную',
+                                 callback_data=MenuCallBack(level=0, menu_name='main').pack()),
+            InlineKeyboardButton(text='Заказать',
+                                 callback_data=MenuCallBack(level=0, menu_name='order').pack()),
+        ]
+        return keyboard.row(*row2).as_markup()
+    else:
+        keyboard.add(
+            InlineKeyboardButton(text='На главную',
+                                 callback_data=MenuCallBack(level=0, menu_name='main').pack()),
+        )
+        return keyboard.adjust(*sizes).as_markup()
